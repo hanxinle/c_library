a5729bb5a5000ba90a44ff32780c3565e8a7a623
Benjamin <hanxloop@foxmail.com>
2017-09-04 19:15:11 +0800

    Add files via upload

--- src/tree.c
+++ src/tree.c
@@ -3,12 +3,13 @@
 #include <stdlib.h>
 #include "fatal.h"
 #include <stdio.h>
+
+
 struct TreeNode  {
   ElementType Element;
   SearchTree  Left;
   SearchTree  Right;
 };
-
 
 SearchTree  MakeEmpty( SearchTree T )  {
   if( T != NULL )
@@ -20,33 +21,25 @@ SearchTree  MakeEmpty( SearchTree T )  {
   return NULL;
 }
 
-
-
 Position   Find( ElementType X, SearchTree T )   {
   if( T == NULL )
     return NULL;
-  if( X < T->Element )
-    return Find( X, T->Left );
-  else
-    if( X > T->Element )
+  if  ( X < T->Element )
+     	return Find( X, T->Left );
+  else if( X > T->Element )
       return Find( X, T->Right );
-    else
+  else
       return T;
 }
-
-
 
 Position   FindMin( SearchTree T )  {
   if( T == NULL )
     return NULL;
-  else
-    if( T->Left == NULL )
+  else if( T->Left == NULL )
       return T;
-    else
+  else
       return FindMin( T->Left );
 }
-
-
 
 Position  FindMax( SearchTree T )    {
   if( T != NULL )
@@ -55,8 +48,6 @@ Position  FindMax( SearchTree T )    {
 
   return T;
 }
-
-
 
 SearchTree Insert( ElementType X, SearchTree T )  {
   if( T == NULL )  {
@@ -69,32 +60,26 @@ SearchTree Insert( ElementType X, SearchTree T )  {
       T->Left = T->Right = NULL;
     }
   }
-  else
-    if( X < T->Element )
+  else if( X < T->Element )
       T->Left = Insert( X, T->Left );
-    else
-      if( X > T->Element )
+  else if( X > T->Element )
 	T->Right = Insert( X, T->Right );
   /* Else X is in the tree already; we'll do nothing */
 
   return T;  /* Do not forget this line!! */
 }
 
-
-
 SearchTree   Delete( ElementType X, SearchTree T )  {
   Position TmpCell;
 
   if( T == NULL )
     Error( "Element not found" );
-  else
-    if( X < T->Element )  /* Go left */
+  else  if( X < T->Element )  /* Go left */
       T->Left = Delete( X, T->Left );
-    else
-      if( X > T->Element )  /* Go right */
-	T->Right = Delete( X, T->Right );
-      else  /* Found element to be deleted */
-	if( T->Left && T->Right ) /* Two children */  {
+  else  if( X > T->Element )  /* Go right */
+			T->Right = Delete( X, T->Right );
+	else 	if ( T->Left && T->Right ) /* Two children */  {
+		/* Found element to be deleted */
 	  /* Replace with smallest in right subtree */
 	  TmpCell = FindMin( T->Right );
 	  T->Element = TmpCell->Element;
@@ -111,11 +96,8 @@ SearchTree   Delete( ElementType X, SearchTree T )  {
 
   return T;
 }
-/* END */
 
-ElementType
-Retrieve( Position P )
-{
+ElementType Retrieve( Position P )  {
   return P->Element;
 }
 
@@ -123,10 +105,10 @@ void PrintElement(ElementType X)  {
   printf("%d\n",X);
 }
 
-
 void PrintTree (SearchTree T)  {
   if (T != NULL)  {
     PrintTree(T->Left);
     PrintElement(T->Element);
     PrintTree(T->Right);
-  }}
+  }
+}
